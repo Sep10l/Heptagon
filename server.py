60bd2bdec1cb21c0c98bb7715c1fa269c2ef40d7
Sep10l <samsepiol0401@gmail.com>
2025-10-31 19:37:10 +0200

    added server.py

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import socket
+
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+server_sock.bind(('0.0.0.0', 12345))
+server_sock.listen()
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+client_sock.connect(('server_ip_address', 12345))
+
+
