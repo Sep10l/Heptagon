3b3fcc400f1a48ba54e97c248ce9034b5ed9444c
Sep1ol <samsepiol0401@gmail.com>
2025-10-31 19:41:50 +0200

    testing SSH

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import socket
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 server_sock.bind(('0.0.0.0', 12345))
 server_sock.listen()
+a = 2
 
 client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 client_sock.connect(('server_ip_address', 12345))
